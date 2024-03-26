9a7e21c91ce1e4103206f01a4300e999c50404d0
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-03-26 09:37:24 -0700

    adjusting lint config

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+  return (
+    <header>
+      <Link to='/'>Fast React Pizza Bros</Link>
+      <p>Taco Joe</p>
+    </header>
+  )
+}
+
+export default Header
