d69906d9b4b6cc33c50958dc4c3963709d0b2961
Brendan McBryan <brendan.mcbryan@gmail.com>
2024-03-28 14:39:48 -0700

    updating data with fetcher.Form

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -10,6 +10,7 @@ import {
 
 import OrderItem from "./OrderItem";
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 function Order() {
   const order = useLoaderData();
@@ -23,7 +24,7 @@ function Order() {
     [fetcher],
   );
 
-  console.log(fetcher.data);
+  // console.log(fetcher.data);
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -90,6 +91,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
